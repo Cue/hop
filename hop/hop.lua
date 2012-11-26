739d4e05e7172aa0568630f8b525663fd10f3933
Daniel Gross <daniel@greplin.com>
2012-11-26 13:52:32 -0800

    don't strip output

--- hop/hop.lua
+++ hop/hop.lua
@@ -42,14 +42,10 @@ function removePrefix(str, pat)
 end
 
 
-function backtick(cmd, raw)
+function backtick(cmd)
   local f = assert(io.popen(cmd, 'r'))
   local s = assert(f:read('*a'))
   f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
   return s
 end
 
